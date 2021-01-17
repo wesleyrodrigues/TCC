adc619294d6866dea5e9581194fe26337d2e8627
wesleyrodrigues <wesleyrg2000@gmail.com>
2021-01-17 01:24:11 -0400

    adicionado scritps de enviar emails

---
+++ App/Testes/Email/teste1.py
@@ -0,0 +1,51 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+# conexão com os servidores do google
+smtp_ssl_host = 'smtp.gmail.com'
+smtp_ssl_port = 465
+
+# username ou email para logar no servidor
+username = 'alfaedu001@gmail.com'
+password = '6eXQ|&Mm3l@4r}B&4'
+
+from_addr = 'alfaedu001@gmail.com'
+# TODO acesso a BD e email do professor default 
+to_addrs = ['default@gmail.com']
+
+
+# a biblioteca email possuí vários templates
+# para diferentes formatos de mensagem
+# neste caso usaremos MIMEText para enviar
+# somente texto
+message = MIMEText('Hello World teste 1')
+message['subject'] = 'Hello'
+message['from'] = from_addr
+message['to'] = ', '.join(to_addrs)
+
+
+try:
+    # conectaremos de forma segura usando SSL
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+
+    # para interagir com um servidor externo precisaremos
+    # fazer login nele
+    server.login(username, password)
+
+    server.sendmail(from_addr, to_addrs, message.as_string())
+
+    server.quit()
+except Exception as e:
+    e = str(e)
+    internetError = "[Errno 11001] getaddrinfo failed"
+    passwordError = "Username and Password not accepted."
+    
+    if internetError in e:
+        print("Erro de internet")
+    elif passwordError in e:
+        print("Erro na conta AlfaEdu, contate suporte")
+    else:
+        print(e)
+        print("outro erro")    
---
+++ App/Testes/Email/teste2.py
@@ -0,0 +1,78 @@
+# https://humberto.io/pt-br/blog/enviando-e-recebendo-emails-com-python/
+# https://medium.com/vacatronics/como-enviar-email-com-python-e43a9af7f672
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+# Configuração
+# conexão com os servidores do google
+host = 'smtp.gmail.com'
+port = 465
+
+# username ou email para logar no servidor
+user = 'alfaedu001@gmail.com'
+# TODO criptografar senha
+
+password = '6eXQ|&Mm3l@4r}B&4'
+
+try:
+    # Criando objeto
+    print('Criando objeto servidor...')
+    # conectando de forma segura usando SSL
+    server = smtplib.SMTP_SSL(host, port)
+
+    # server.ehlo()
+    # server.starttls()
+
+    # Login com servidor
+    print('Login...')
+    server.login(user, password)
+
+    # Criando mensagem
+    message = 'Teste com imagem'
+    print('Criando mensagem...')
+    email_msg = MIMEMultipart()
+    email_msg['From'] = user
+
+    # TODO acessar BD
+    email_msg['To'] = 'default@hotmail.com'
+    email_msg['Subject'] = 'enviando imagem'
+    print('Adicionando texto...')
+    email_msg.attach(MIMEText(message, 'plain'))
+
+    print('Obtendo arquivo...')
+    #TODO verificar localização do arquivo depois
+    filename = 'binoculo.png' 
+    filepath = 'binoculo.png'
+    attachment = open(filepath, 'rb')
+
+    print('Lendo arquivo...')
+    att = MIMEBase('application', 'octet-stream')
+    att.set_payload(attachment.read())
+    encoders.encode_base64(att)
+    att.add_header('Content-Disposition', f'attachment; filename= {filename}')
+
+    attachment.close()
+    print('Adicionando arquivo ao email...')
+    email_msg.attach(att)
+
+    # Enviando mensagem
+    print('Enviando mensagem...')
+    server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+    print('Mensagem enviada!')
+    server.quit()
+
+except Exception as e:
+    e = str(e)
+    internetError = "[Errno 11001] getaddrinfo failed"
+    passwordError = "Username and Password not accepted."
+    
+    if internetError in e:
+        print("Erro de internet")
+    elif passwordError in e:
+        print("Erro na conta AlfaEdu, contate suporte")
+    else:
+        print(e)
+        print("outro erro")    
