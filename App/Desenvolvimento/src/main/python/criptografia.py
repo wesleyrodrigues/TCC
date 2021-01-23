2c6c19ec313600c610e41dba5b1064498cf440fc
wesleyrodrigues <wesleyrg2000@gmail.com>
2021-01-22 23:51:47 -0400

    Downgrade para python 3.6

--- App/Desenvolvimento/src/main/python/criptografia.py
+++ App/Desenvolvimento/src/main/python/criptografia.py
@@ -1,10 +1,12 @@
-import bcrypt
+# import bcrypt
 
-
+#TODO não funciona em produção
 class Cript():
     def criptografa_senha(senha):
-        hashed = bcrypt.hashpw(senha.encode('utf_8'), bcrypt.gensalt())
-        return hashed.decode("utf-8")
+        # hashed = bcrypt.hashpw(senha.encode('utf_8'), bcrypt.gensalt())
+        # return hashed.decode("utf-8")
+        return senha
 
     def verifica_senha(senha, senha_cript):
-        return bcrypt.checkpw(senha.encode('utf_8'), senha_cript)
+        # return bcrypt.checkpw(senha.encode('utf_8'), senha_cript)
+        return senha
