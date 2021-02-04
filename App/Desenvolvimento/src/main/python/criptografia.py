82a36b7c829ddbaafe3442d0b4039d4e1fc418c7
wesleyrodrigues <wesleyrg2000@gmail.com>
2021-02-03 20:16:26 -0400

    agora criptografia funciona

--- App/Desenvolvimento/src/main/python/criptografia.py
+++ App/Desenvolvimento/src/main/python/criptografia.py
@@ -2,13 +2,21 @@
 import hashlib
 import os
 
-#TODO não funciona em produção
 class Cript():
     def criptografa_senha(senha):
+        
+        salt = os.urandom(32)
+        key = hashlib.pbkdf2_hmac('sha256', senha.encode('latin-1'), salt, 100000)
+
+        senha_cript = salt + key
         # hashed = bcrypt.hashpw(senha.encode('utf_8'), bcrypt.gensalt())
         # return hashed.decode("utf-8")
-        return senha
+        return senha_cript.decode('latin-1')
 
     def verifica_usuario_e_senha(usuario, senha, senha_cript):
-        # return bcrypt.checkpw(senha.encode('utf_8'), senha_cript)
-        return senha
+        senha_cript = senha_cript.encode('latin-1')
+        salt = senha_cript[:32]
+        key = senha_cript[32:]
+        new_key = hashlib.pbkdf2_hmac('sha256', senha.encode('latin-1'), salt, 100000)
+        print(new_key)
+        return key == new_key
